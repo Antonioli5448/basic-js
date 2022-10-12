3dcb1049f2d61750ee08d088133bbad6bf0df5b2
Anton Kazachenka <Kazachenkoanton98@gmail.com>
2022-10-12 17:10:45 +0300

    feat:complete one task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,8 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let array = []
+  for(let i = 0;i<matrix.length;i++){
+    for(let j = 0; j < matrix[i].length;j++){
+      if(matrix[i][j] === '^^'){
+          array.push(matrix[i][j])
+      }
+    }
+  }
+  return array.length ? array.length : 0
   // remove line with error and write your code here
 }
 
